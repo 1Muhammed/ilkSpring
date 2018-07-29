068a8a36654816527a4ea73cb1be4cdf8da297c4
vektorel <1muhammed.korucu@gmail.com>
2018-07-29 16:51:01 +0300

    5. yayın

---
+++ src/main/java/com/korucu/controllers/controlSinif.java
@@ -0,0 +1,20 @@
+package com.korucu.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class controlSinif {
+
+	@RequestMapping("/MainPage")
+	@ResponseBody
+	public String anasayfa () {
+		
+		return "AnaSayfaya Git ===>";
+		
+	}
+	
+	
+	
+}
