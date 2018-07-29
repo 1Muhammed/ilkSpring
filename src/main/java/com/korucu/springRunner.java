5ee6060d63a8e9777d9f1a022e7fdf871ad2e3e6
vektorel <1muhammed.korucu@gmail.com>
2018-07-29 16:17:06 +0300

    3.yayın

---
+++ src/main/java/com/korucu/springRunner.java
@@ -0,0 +1,18 @@
+package com.korucu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springRunner {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(springRunner.class, args);
+		
+		
+		
+		
+	}
+
+}
